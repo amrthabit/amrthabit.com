cdb1e0f0b6af0b4a4e5c9e671a0e9c1bebc4066f
Amr <amrthabi7@gmail.com>
2023-06-12 03:21:51 -0400

    refactor contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TitleComponent } from './title/title.component';
 import { LinksComponent } from './links/links.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProjectsComponent } from './projects/projects.component';
     TitleComponent,
     LinksComponent,
     AboutComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
