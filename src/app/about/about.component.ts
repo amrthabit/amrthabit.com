a3e415d9033ec928d83497172cc6b2979a5e22cb
Amr <amrthabi7@gmail.com>
2023-06-23 21:55:01 -0400

    add major style changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-
+  scroll(id: string) {
+    document.getElementById(id)?.scrollIntoView();
+  }
 }
