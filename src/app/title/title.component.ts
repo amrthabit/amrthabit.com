fd806c66e11267935e3456b073e24136f2fb2bd1
Amr <amrthabi7@gmail.com>
2024-03-08 00:53:40 -0500

    goofy ahh commit

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss']
+  styleUrls: ['./title.component.scss'],
 })
 export class TitleComponent {
-
+  //  add .glowing class to .name after 2 seconds
+  // ngAfterViewInit() {
+  //   setTimeout(() => {
+  //     document.querySelector('.name')?.classList.add('glowing');
+  //   }, 500);
+  //   setTimeout(() => {
+  //     document.querySelector('.name')?.classList.remove('glowing');
+  //   }, 2000);
+  // }
+  // make it cyle between glowing and not glowing
+  // ngAfterViewInit() {
+  //   setInterval(() => {
+  //     document.querySelector('.name')?.classList.toggle('glowing');
+  //   }, 1000);
+  // }
 }
